d7e4167c091b8c31d18532d5f84114d6f7eda8d4
Ummadi Nithin <ummadinithin999@gmail.com>
2025-04-11 15:37:17 +0530

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -20,7 +20,7 @@ const Signup = () => {
     setError('');
   
     try {
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch('https://job-tracker-backend-2-8sat.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
